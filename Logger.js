0c635bc3ab56383ab39ea46806466d616ce71057
Max <max@guavaapps.com>
2026-02-08 01:23:22 +0000

    logging utils

--- Logger.js
+++ Logger.js
@@ -1,15 +1,15 @@
 function Debug(ctx, ...messages) {
-    console.debug(`debug:[${ctx.method.toUpperCase()}] ${ctx.route} ${messages}`)
+    console.log(`debug:[${ctx.method.toUpperCase()}] ${ctx.route} ${messages}`)
 }
 
 function Error(ctx, ...messages) {
-    console.error(`error:[${ctx.method.toUpperCase()}] ${ctx.route} ${messages}`)
+    console.log(`error:[${ctx.method.toUpperCase()}] ${ctx.route} ${messages}`)
 }
 
 function Info(ctx, ...messages) {
-    console.info(`info:[${ctx.method.toUpperCase()}] ${ctx.route} ${messages}`)
+    console.log(`info:[${ctx.method.toUpperCase()}] ${ctx.route} ${messages}`)
 }
 
 function Log(ctx, ...messages) {
-    console.error(`[${ctx.method.toUpperCase()}] ${ctx.route} ${messages}`)
+    console.log(`[${ctx.method.toUpperCase()}] ${ctx.route} ${messages}`)
 }
